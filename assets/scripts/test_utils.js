ccfb935b01359a981039628911b1fb59d8ae803d
DejonGill <dejonjgill@gmail.com>
2016-10-25 11:13:15 -0700

    Added gitignore

--- assets/scripts/test_utils.js
+++ assets/scripts/test_utils.js
@@ -1,19 +1,77 @@
 'use strict';
-var currentTest = 0;
-var tests = [
-  {
-    title: 'Basic Variable',
-    prompt: 'hello',
-    correctAnswer: 'hello'
+
+function TestUtils(tests) {
+  var tests = tests;
+  var testUtils = {
+    testRunner: function() {
+      this.showTestCount(tests);
+      if (tests.currentTest < tests.units.length) {
+        this.checkAnswer(tests.units[tests.currentTest], tests);
+      } else {
+        this.congratulate();
+      }
+    },
+
+    sendNotAFunctionError: function(test) {
+      this.sendMessage(`Expected ${test.target} to be a function, but instead got ${typeof window[test['target']]}.`, 'red')
+    },
+
+    checkAnswerInVariable: function(test) {
+      if (window[test['target']] === test.expected) {
+        this.incrementTestCount(tests);
+        this.testRunner(tests);
+      } else {
+        this.sendMessage(test.prompt, 'red');
+      }
+    },
+
+    checkFunctionAnswer: function(test, tests) {
+      if (window[test['target']]() === test.expected) {
+        this.incrementTestCount(tests);
+        this.testRunner(tests);
+      } else {
+        this.sendMessage(test.prompt, 'red');
+      }
+    },
+
+    checkAnswer: function(test, tests) {
+      if (test.type === 'var') {
+        this.checkAnswerInVariable(test, tests);
+      }
+      if (test.type === 'func') {
+        if (typeof window[test['target']] !== 'function') {
+          return this.sendNotAFunctionError(test)
+        }
+        this.checkFunctionAnswer(test, tests);
+      }
+    },
+
+    incrementTestCount: function(tests) {
+      tests.currentTest++;
+      this.showTestCount(tests);
+    },
+
+    showTestCount: function(tests) {
+      var testsLeft = 'Tests left: ' + (tests.units.length - tests.currentTest).toString();
+      document.getElementById('test_count').textContent = testsLeft;
+    },
+
+    congratulate: function() {
+      this.sendMessage('That\'s it!', 'green');
+    },
+
+    makeElement: function(text, color) {
+      var newEle = document.createElement('h3');
+      newEle.textContent = text;
+      newEle.style.color = color;
+      return newEle;
+    },
+
+    sendMessage: function(message, color) {
+      // document.getElementById('tests').innerHTML = '';
+      var newMessage = this.makeElement(message, color);
+      document.getElementById('tests').prepend(newMessage);
+    }
   }
-];
-
-function showPrompt(test) {
-  var title = document.createElement('p');
-  title.textContent = test.title;
-}
-
-function showNextTest() {
-  var targetDiv = document.getElementById('tests');
-  targetDiv.appendChild(showPrompt(tests[currentTest]))
+  return testUtils;
 }
