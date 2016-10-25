4f9d9101d8efb10489ba3798c78d7a013adb6147
DejonGill <dejonjgill@gmail.com>
2016-10-25 12:31:25 -0700

    First batch of basic tests complete

--- assets/scripts/testing/tests_part_one.js
+++ assets/scripts/testing/tests_part_one.js
@@ -18,6 +18,34 @@ var t = {
       expected: 10
     },
     {
+      title: 'thisYear divided by thisMonth',
+      prompt: 'There should be a variable called dividedBy with the value of thisYear divided by thisMonth',
+      target: 'dividedBy',
+      type: 'var',
+      expected: (2016 / 10)
+    },
+    {
+      title: 'getting the remainder',
+      prompt: 'There should be a variable called remainder that is the remainder from dividing 279 by 7',
+      target: 'remainder',
+      type: 'var',
+      expected: (279 % 7)
+    },
+    {
+      title: 'Multiplying',
+      prompt: 'There should be a variable called eightteenSquared that holds the value of eightteen times eightteen',
+      target: 'eightteenSquared',
+      type: 'var',
+      expected: (18 * 18)
+    },
+    {
+      title: 'fakeStreet is a string',
+      prompt: 'There should be a varibale called fakeStreet with the value of 123 Market Street',
+      target: 'fakeStreet',
+      type: 'var',
+      expected: '123 Market Street'
+    },
+    {
       title: 'thisCity is a string',
       prompt: 'There should be a variable called thisCity with the value San Francisco',
       target: 'thisCity',
@@ -32,18 +60,25 @@ var t = {
       expected: 'California'
     },
     {
-      title: 'A function called greet',
-      prompt: 'function greet returns "hello world"',
-      target: 'greet',
-      type: 'func',
-      expected: 'hello world'
+      title: 'fakeAddress should be a string.',
+      prompt: 'There should be a varibale called fakeAddress with the value of 123 Market Street San Francisco, California Concat the strings, don\'t cheat yourself!',
+      target: 'fakeAddress',
+      type: 'var',
+      expected: '123 Market Street San Francisco, California'
     },
     {
-      title: 'division',
-      prompt: 'There should be a variable named divided that holds the value 4 divided by 3',
-      target: 'divided',
-      type: 'var',
-      expected: 4 / 3
-    }
+      title: 'basic function',
+      prompt: 'There should be a function called greet that returns the string "Hello World!"',
+      target: 'greet',
+      type: 'func',
+      expected: 'Hello World!'
+    },
+    {
+      title: 'function with one parameter',
+      prompt: 'There should be a function called greetName that takes a name as a parameter. When invoked, it should return the string "Hello <name>" where <name> is the parameter passed in.',
+      target: 'greetName',
+      type: 'func',
+      expected: 'Hello'
+    },
   ]
 };
