f8ff403a2f657fd5efcd7816005bddbb3b5027ba
DejonGill <dejonjgill@gmail.com>
2016-10-25 22:54:17 -0700

    Beginning part two

--- assets/scripts/testing/tests_part_one.js
+++ assets/scripts/testing/tests_part_one.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var t = {
+var partOneTests = {
   currentTest: 0,
   units: [
     {
---
+++ assets/scripts/testing/tests_part_two.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var partTwoTests = {
+  currentTest: 0,
+  units: [
+    {
+      title: 'A function that returns an array',
+      prompt: 'There should be a function called arrayConstruc',
+      target: 'listOfNums',
+      type: 'var',
+      expected: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  ]
+}
