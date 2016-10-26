8c2563efbdcdc8128d83c20bc261a5f63db38a14
DejonGill <dejonjgill@gmail.com>
2016-10-26 11:43:21 -0700

    Changed prompt to be a Yoda object. I think people will like that.

--- assets/scripts/testing/bonus_round.js
+++ assets/scripts/testing/bonus_round.js
@@ -1,14 +1,16 @@
 describe('Using Objects', function() {
 
-  describe('a variable called inReview', function() {
+  describe('a variable called yoda', function() {
 
     it('should exist', function () {
-      expect(inReview).to.exist;
+      expect(yoda).to.exist;
     });
     it('should be an object', function () {
-      expect(typeof inReview).to.equal('object');
+      expect(typeof yoda).to.equal('object');
     });
-    it('should have each of the following keys: ')
+    it('should have each of the following keys: species, affiliation, occupation, forceUser, lightSide, darkSide. The values either come from the yoda wikipedia article\'s sidebar, or from having watched Star Wars.', function() {
+      expect(yoda).to.have.all.keys(['species', 'affiliation', 'occupation', 'forceUser', 'lightSide', 'darkSide']);
+    });
   });
 
 });
